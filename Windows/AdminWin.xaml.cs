863fd40602ce4f07bd4a7f746d43276346a96838
Tim773 <73285255+Tim773@users.noreply.github.com>
2021-06-02 13:51:35 +0300

    эээ

--- Windows/AdminWin.xaml.cs
+++ Windows/AdminWin.xaml.cs
@@ -16,292 +16,305 @@ using static ELjournal.AppData.DataClass;
 
 namespace ELjournal.Windows
 {
-	/// <summary>
-	/// Логика взаимодействия для AdminWin.xaml
-	/// </summary>
-	public partial class AdminWin : Window
-	{
-		//номер страницы
-		public int Page { get; set; } = 0;
-		public int RowAll { get; set; } = 0;
-		public int RowAll2 { get; set; } = 0;
-		public int ShowedRows { get; set; } = 0;
+    /// <summary>
+    /// Логика взаимодействия для AdminWin.xaml
+    /// </summary>
+    public partial class AdminWin : Window
+    {
+        //номер страницы
+        public int Page { get; set; } = 0;
+        public int RowAll { get; set; } = 0;
+        public int RowAll2 { get; set; } = 0;
+        public int ShowedRows { get; set; } = 0;
 
 
-		public AdminWin()
-		{
-			InitializeComponent();
-			cb_NumItems.ItemsSource = new List<string>()
-			{
-				"10",
-				"20",
-				"30",
-				"All"
-			};
-			cb_NumItems.SelectedIndex = 0;
+        public AdminWin()
+        {
+            InitializeComponent();
+            cb_NumItems.ItemsSource = new List<string>()
+            {
+                "10",
+                "20",
+                "30",
+                "All"
+            };
+            cb_NumItems.SelectedIndex = 0;
 
-			Update();
-		}
+            Update();
+        }
 
-		public void Update()
-		{
-			var datasourse = context.Students.ToList().Where(i => i.available == 1);
-			var datasourse2 = context.Teachers.ToList().Where(i => i.available == 1);
+        public void Update()
+        {
+            var datasourse = context.Students.ToList().Where(i => i.available == 1);
+            var datasourse2 = context.Teachers.ToList().Where(i => i.available == 1);
 
-			RowAll = datasourse.Count();
-			RowAll2 = datasourse2.Count();
-			switch (cb_NumItems.SelectedIndex)
-			{
-				case 0:
-					datasourse = datasourse.Skip(Page * 10).Take(10).ToList();
-					datasourse2 = datasourse2.Skip(Page * 10).Take(10).ToList();
-					if (rb_Prep.IsChecked == true)
-					{
-						btn_Next.IsEnabled = datasourse2.Count() >= 10;
+            RowAll = datasourse.Count();
+            RowAll2 = datasourse2.Count();
+            switch (cb_NumItems.SelectedIndex)
+            {
+                case 0:
+                    datasourse = datasourse.Skip(Page * 10).Take(10).ToList();
+                    datasourse2 = datasourse2.Skip(Page * 10).Take(10).ToList();
+                    if (rb_Prep.IsChecked == true)
+                    {
+                        btn_Next.IsEnabled = datasourse2.Count() >= 10;
 
-					}
-					else if (rb_Stud.IsChecked == true)
-					{
-						btn_Next.IsEnabled = datasourse.Count() >= 10;
+                    }
+                    else if (rb_Stud.IsChecked == true)
+                    {
+                        btn_Next.IsEnabled = datasourse.Count() >= 10;
 
-					}
-					break;
-				case 1:
-					datasourse = datasourse.Skip(Page * 20).Take(20).ToList();
-					datasourse2 = datasourse2.Skip(Page * 20).Take(20).ToList();
-					if (rb_Prep.IsChecked == true)
-					{
-						btn_Next.IsEnabled = datasourse2.Count() >= 20;
+                    }
+                    break;
+                case 1:
+                    datasourse = datasourse.Skip(Page * 20).Take(20).ToList();
+                    datasourse2 = datasourse2.Skip(Page * 20).Take(20).ToList();
+                    if (rb_Prep.IsChecked == true)
+                    {
+                        btn_Next.IsEnabled = datasourse2.Count() >= 20;
 
-					}
-					else if (rb_Stud.IsChecked == true)
-					{
-						btn_Next.IsEnabled = datasourse.Count() >= 20;
+                    }
+                    else if (rb_Stud.IsChecked == true)
+                    {
+                        btn_Next.IsEnabled = datasourse.Count() >= 20;
 
-					}
+                    }
 
-					break;
-				case 2:
-					datasourse = datasourse.Skip(Page * 30).Take(30).ToList();
-					datasourse2 = datasourse2.Skip(Page * 30).Take(30).ToList();
-					if (rb_Prep.IsChecked == true)
-					{
-						btn_Next.IsEnabled = datasourse2.Count() >= 30;
+                    break;
+                case 2:
+                    datasourse = datasourse.Skip(Page * 30).Take(30).ToList();
+                    datasourse2 = datasourse2.Skip(Page * 30).Take(30).ToList();
+                    if (rb_Prep.IsChecked == true)
+                    {
+                        btn_Next.IsEnabled = datasourse2.Count() >= 30;
 
-					}
-					else if (rb_Stud.IsChecked == true)
-					{
-						btn_Next.IsEnabled = datasourse.Count() >= 30;
+                    }
+                    else if (rb_Stud.IsChecked == true)
+                    {
+                        btn_Next.IsEnabled = datasourse.Count() >= 30;
 
-					}
+                    }
 
-					break;
-				case 3:
-					datasourse = datasourse.ToList();
-					datasourse2 = datasourse2.ToList();
-					btn_Next.IsEnabled = false;
+                    break;
+                case 3:
+                    datasourse = datasourse.ToList();
+                    datasourse2 = datasourse2.ToList();
+                    btn_Next.IsEnabled = false;
 
-					break;
+                    break;
 
-				default:
-					break;
-			}
-			//ShowedRows = datasourse.Count();
-			lb_Students.ItemsSource = datasourse;
-			lb_Teachers.ItemsSource = datasourse2;
-			btn_Back.IsEnabled = Page != 0;
+                default:
+                    break;
+            }
+            //ShowedRows = datasourse.Count();
+            lb_Students.ItemsSource = datasourse;
+            lb_Teachers.ItemsSource = datasourse2;
+            btn_Back.IsEnabled = Page != 0;
 
-		}
+            
 
-		private void btn_Add_Click(object sender, RoutedEventArgs e)
-		{
-			AddUser addUser = new AddUser();
-			addUser.ShowDialog();
-		}
+        }
 
-		private void btn_Del_Click(object sender, RoutedEventArgs e)
-		{
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddUser addUser = new AddUser();
+            addUser.ShowDialog();
+        }
 
-			try
-			{
-				if (rb_Stud.IsChecked == true)
-				{
-					var itm = lb_Students.SelectedItem;
-					if (itm == null)
-					{
-						return;
-					}
-					if (MessageBox.Show("Строка пользователя будет удалена из таблицы. Желаете продолжить?", "Удаление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-					{
-						int del = (lb_Students.SelectedItem as Students).idStud;
+        private void btn_Del_Click(object sender, RoutedEventArgs e)
+        {
 
-						Students students = context.Students.Where(i => i.idStud == del).FirstOrDefault();
-						students.available = 0;
-						context.SaveChanges();
-						ListBox_Loaded(sender, e);
+            try
+            {
+                if (rb_Stud.IsChecked == true)
+                {
+                    var itm = lb_Students.SelectedItem;
+                    if (itm == null)
+                    {
+                        return;
+                    }
+                    if (MessageBox.Show("Строка пользователя будет удалена из таблицы. Желаете продолжить?", "Удаление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        int del = (lb_Students.SelectedItem as Students).idStud;
 
-
-					}
-					else return;
-
-				}
-				else if (rb_Prep.IsChecked == true)
-				{
-					var itm = lb_Teachers.SelectedItem;
-					if (itm == null)
-					{
-						return;
-					}
-					if (MessageBox.Show("Строка пользователя будет удалена из таблицы. Желаете продолжить?", "Удаление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-					{
-						int del = (lb_Teachers.SelectedItem as Teachers).idTeach;
-
-						Teachers teachers = context.Teachers.Where(i => i.idTeach == del).FirstOrDefault();
-						teachers.available = 0;
-						context.SaveChanges();
-						ListBox_Loaded(sender, e);
+                        Students students = context.Students.Where(i => i.idStud == del).FirstOrDefault();
+                        students.available = 0;
+                        context.SaveChanges();
+                        ListBox_Loaded(sender, e);
 
 
-					}
-					else return;
-				}
+                    }
+                    else return;
 
-				Update();
+                }
+                else if (rb_Prep.IsChecked == true)
+                {
+                    var itm = lb_Teachers.SelectedItem;
+                    if (itm == null)
+                    {
+                        return;
+                    }
+                    if (MessageBox.Show("Строка пользователя будет удалена из таблицы. Желаете продолжить?", "Удаление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        int del = (lb_Teachers.SelectedItem as Teachers).idTeach;
 
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message, "Что-то пошло не так!");
-			}
+                        Teachers teachers = context.Teachers.Where(i => i.idTeach == del).FirstOrDefault();
+                        teachers.available = 0;
+                        context.SaveChanges();
+                        ListBox_Loaded(sender, e);
+
+
+                    }
+                    else return;
+                }
+
+                Update();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Что-то пошло не так!");
+            }
 
 
 
-		}
-		private void btn_Next_Click(object sender, RoutedEventArgs e)
-		{
-			btn_Back.IsEnabled = true;
-			Page++;
-			Update();
-		}
+        }
+        private void btn_Next_Click(object sender, RoutedEventArgs e)
+        {
+            btn_Back.IsEnabled = true;
+            Page++;
+            Update();
+        }
 
-		private void btn_Back_Click(object sender, RoutedEventArgs e)
-		{
-			Page--;
-			Update();
-		}
+        private void btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            Page--;
+            Update();
+        }
 
-		private void ListBox_Loaded(object sender, RoutedEventArgs e)
-		{
+        private void ListBox_Loaded(object sender, RoutedEventArgs e)
+        {
 
-			if (rb_Prep.IsChecked == true)
-			{
-				lb_Teachers.Visibility = Visibility.Visible;
-				lb_Students.Visibility = Visibility.Collapsed;
+            if (rb_Prep.IsChecked == true)
+            {
+                lb_Teachers.Visibility = Visibility.Visible;
+                lb_Students.Visibility = Visibility.Collapsed;
 
-			}
-			else if (rb_Stud.IsChecked == true)
-			{
-				lb_Teachers.Visibility = Visibility.Collapsed;
-				lb_Students.Visibility = Visibility.Visible;
+            }
+            else if (rb_Stud.IsChecked == true)
+            {
+                lb_Teachers.Visibility = Visibility.Collapsed;
+                lb_Students.Visibility = Visibility.Visible;
 
-			}
-		}
+            }
+        }
 
-		private void rb_Stud_Click(object sender, RoutedEventArgs e)
-		{
-			lb_Teachers.Visibility = Visibility.Collapsed;
-			lb_Students.Visibility = Visibility.Visible;
-			Page = 0;
-			Update();
-		}
+        private void rb_Stud_Click(object sender, RoutedEventArgs e)
+        {
+            lb_Teachers.Visibility = Visibility.Collapsed;
+            lb_Students.Visibility = Visibility.Visible;
 
-		private void rb_Prep_Click(object sender, RoutedEventArgs e)
-		{
-			lb_Teachers.Visibility = Visibility.Visible;
-			lb_Students.Visibility = Visibility.Collapsed;
-			Page = 0;
-			Update();
-		}
+            GR_BOX.Visibility = Visibility.Visible;
+            GR_BOX2.Visibility = Visibility.Collapsed;
 
-		private void cb_NumItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			Page = 0;
-			Update();
-		}
+            Page = 0;
+            Update();
+        }
 
-		private void lb_Students_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-		{
-			if (lb_Students.SelectedItem is Students students)
-			{
-				EditStudent edit = new EditStudent(students);
-				edit.ShowDialog();
-				Update();
-			}
+        private void rb_Prep_Click(object sender, RoutedEventArgs e)
+        {
+            lb_Teachers.Visibility = Visibility.Visible;
+            lb_Students.Visibility = Visibility.Collapsed;
 
-		}
+            GR_BOX2.Visibility = Visibility.Visible;
+            GR_BOX.Visibility = Visibility.Collapsed;
 
-		private void lb_Teachers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-		{
-			if (lb_Teachers.SelectedItem is Teachers teachers)
-			{
-				EditTeacher edit = new EditTeacher(teachers);
-				edit.ShowDialog();
-				Update();
-			}
-		}
+            Page = 0;
+            Update();
+        }
 
-		private void ResetSearch(object sende, RoutedEventArgs e)
-		{
-			FamSearch1.Text = "";
-			ImySearch1.Text = "";
-			PatrSearch1.Text = "";
+        private void cb_NumItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Page = 0;
+            Update();
+        }
 
-			lb_Students.ItemsSource = context.Students.ToList();
-		}
-		private void Search(object sender, RoutedEventArgs e)
-		{
-			if (rb_Stud.IsChecked == true)
-			{
-				var tabb = context.Students.ToList().Where(i => i.available == 1);
-				if (FamSearch1.Text.Length == 0 && ImySearch1.Text.Length == 0 &&
-					PatrSearch1.Text.Length == 0)
-				{
-					lb_Students.ItemsSource = tabb.ToList();
-					return;
-				}
+        private void lb_Students_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lb_Students.SelectedItem is Students students)
+            {
+                EditStudent edit = new EditStudent(students);
+                edit.ShowDialog();
+                Update();
+            }
 
-				var res = tabb.Where(i => i.lName.Contains(FamSearch1.Text) &&
-									 i.fName.Contains(ImySearch1.Text) &&
-									 i.patronymic.Contains(PatrSearch1.Text)
-									 ).ToList();
-				if (res.Count() != 0)
-					lb_Students.ItemsSource = res;
-				else
-					MessageBox.Show("Внимание!", " Не найдено!",
-						MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
 
-			}
-			else if (rb_Prep.IsChecked == true) 
-			{
-				var tabb = context.Teachers.ToList().Where(i => i.available == 1);
-				if (FamSearch1.Text.Length == 0 && ImySearch1.Text.Length == 0 &&
-					PatrSearch1.Text.Length == 0)
-				{
-					lb_Students.ItemsSource = tabb.ToList();
-					return;
-				}
+        private void lb_Teachers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lb_Teachers.SelectedItem is Teachers teachers)
+            {
+                EditTeacher edit = new EditTeacher(teachers);
+                edit.ShowDialog();
+                Update();
+            }
+        }
 
-				var res = tabb.Where(i => i.lName.Contains(FamSearch1.Text) &&
-									 i.fName.Contains(ImySearch1.Text) &&
-									 i.ptronymic.Contains(PatrSearch1.Text)
-									 ).ToList();
-				if (res.Count() != 0)
-					lb_Students.ItemsSource = res;
-				else
-					MessageBox.Show("Внимание!", " Не найдено!",
-						MessageBoxButton.OK, MessageBoxImage.Warning);
+        private void ResetSearch(object sende, RoutedEventArgs e)
+        {
+            FamSearch1.Text = "";
+            ImySearch1.Text = "";
+            
+            PatrSearch1.Text = "";
 
-			}
-		}
-	}
+            lb_Students.ItemsSource = context.Students.ToList();
+        }
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            if (rb_Stud.IsChecked == true)
+            {
+                var tabb = context.Students.ToList().Where(i => i.available == 1);
+                if (FamSearch10.Text.Length == 0 && ImySearch10.Text.Length == 0 &&
+                    PatrSearch10.Text.Length == 0)
+                {
+                    lb_Students.ItemsSource = tabb.ToList();
+                    return;
+                }
+
+                var res = tabb.Where(i => i.lName.Contains(FamSearch10.Text) &&
+                                     i.fName.Contains(ImySearch10.Text) &&
+                                     i.patronymic.Contains(PatrSearch10.Text)
+                                     ).ToList();
+                if (res.Count() != 0)
+                    lb_Students.ItemsSource = res;
+                else
+                    MessageBox.Show("Внимание!", " Не найдено!",
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else if (rb_Prep.IsChecked == true)
+            {
+                var tabb = context.Teachers.ToList().Where(i => i.available == 1);
+                if (FamSearch10.Text.Length == 0 && 
+                    ImySearch10.Text.Length == 0 &&
+                    PatrSearch10.Text.Length == 0)
+                {
+                    lb_Teachers.ItemsSource = tabb.ToList();
+                    return;
+                }
+
+
+                var res = tabb.Where(i => i.lName.Contains(FamSearch10.Text) &&
+                                     i.fName.Contains(ImySearch10.Text) &&
+                                     i.ptronymic.Contains(PatrSearch10.Text)
+                                     ).ToList();
+                if (res.Count() != 0)
+                    lb_Teachers.ItemsSource = res;
+                else
+                    MessageBox.Show("Внимание!", " Не найдено!",
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+        }
+    }
 }
